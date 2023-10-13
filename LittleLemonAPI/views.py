373e6b901128a092c3229c5177bb37e2b92b469f
seanarch <zhuqingyun@outlook.com>
2023-10-13 12:58:56 -0600

    Add menu-item, category  permission on manager group

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,11 +1,21 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from .models import MenuItem, Category
 from .serializers import MenuItemSerializer, CategorySerializer
- 
 
+class CanPostMenuItemPermission(BasePermission): 
+    def has_permission(self, request, view): 
+        return request.user.groups.filter(name='manager').exists()
+    
+class CanPostCategoryPermission(BasePermission): 
+    def has_object_permission(self, request, view, obj):
+        return request.user.groups.filter(name='manager').exists() 
+ 
 class CategoriesView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    permission_classes = [IsAuthenticated, CanPostMenuItemPermission]
 
 class MenuItemsView(generics.ListCreateAPIView): 
     queryset = MenuItem.objects.all() 
@@ -14,7 +24,11 @@ class MenuItemsView(generics.ListCreateAPIView):
     # filterset_fields = [] 
     # search_fields = [] 
 
+    permission_classes = [IsAuthenticated, CanPostMenuItemPermission]
+
 class SingleCateItemView(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Category.objects.all() 
     serializer_class = CategorySerializer     
+
+    permission_classes = [IsAuthenticated, CanPostMenuItemPermission]
  
